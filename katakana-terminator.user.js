a15b031a002ecbebecd8422277ff5102b64c3e9e
Arnie97 <arnie97@gmail.com>
2017-09-07 17:12:51 +0800

    Limit the length of API requests.

--- katakana-terminator.user.js
+++ katakana-terminator.user.js
@@ -87,13 +87,25 @@ function googleToken(r) {
     for(var a=406644,e=[],h=0,n=0;n<r.length;n++){var o=r.charCodeAt(n);128>o?e[h++]=o:(2048>o?e[h++]=o>>6|192:(55296==(64512&o)&&n+1<r.length&&56320==(64512&r.charCodeAt(n+1))?(o=65536+((1023&o)<<10)+(1023&r.charCodeAt(++n)),e[h++]=o>>18|240,e[h++]=o>>12&63|128):e[h++]=o>>12|224,e[h++]=o>>6&63|128),e[h++]=63&o|128)}function t(r,t){for(var a=0;a<t.length-2;a+=3){var e=(e=t.charAt(a+2))>="a"?e.charCodeAt(0)-87:Number(e),e="+"==t.charAt(a+1)?r>>>e:r<<e;r="+"==t.charAt(a)?r+e&4294967295:r^e}return r}for(r=a,h=0;h<e.length;h++)r+=e[h],r=t(r,"+-a^+6");return r=t(r,"+-3^+b+-f"),0>(r^=3293161072)&&(r=2147483648+(2147483647&r)),(r%=1e6).toString()+"."+(r^a)
 }
 
+// Split word list into chunks to limit the length of API requests
+function chunkTranslate(start, end) {
+    if (start == end) {
+        return;
+    }
+    var text  = queue[0].slice(start, end).join('\n');
+    var nodes = queue[1].slice(start, end);
+    return googleTranslate('ja', 'en', text, nodes);
+}
+
 // Exception handling
 function main(app_name) {
     try {
         scanTextNodes();
-        if (queue[0].length) {
-            googleTranslate('ja', 'en', queue[0].join('\n'), queue[1]);
+        var chunkSize = 200;
+        for (var i = 0; i + chunkSize < queue[0].length; i += chunkSize) {
+            chunkTranslate(i, i + chunkSize);
         }
+        chunkTranslate(i, queue[0].length);
     } catch (e) {
         console.error('{0}: {1}'.format(app_name, e));
     } finally {
